b782610f7d646186ec6a8369b11bfaa07727acc0
João Pedro Merlotti <73497255+jpmerlotti@users.noreply.github.com>
2023-06-08 14:29:01 -0300

    Update zipar.py

--- zipar.py
+++ zipar.py
@@ -1,14 +1,15 @@
-import os
-import zipfile
+import os, zipfile
 
 def zip_directory(directory_path, zip_path):
     with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
         # Adiciona o arquivo index.html ao arquivo zip
+        # Adds the index.html file to the zip file
         index_html_path = os.path.join(directory_path, 'index.html')
         if os.path.exists(index_html_path):
             zipf.write(index_html_path, 'index.html')
         
         # Adiciona a pasta src ao arquivo zip
+        # Add src folder to zip file
         src_path = os.path.join(directory_path, 'src')
         if os.path.exists(src_path):
             for root, dirs, files in os.walk(src_path):
@@ -18,10 +19,13 @@ def zip_directory(directory_path, zip_path):
                     zipf.write(file_path, os.path.join('src', zip_path))
 
 # Diretório da pasta onde estão os arquivos
-diretorio_origem = r'D:\Projetos\HTML\SiteFatec'
+# Directory of the folder where the files are
+origin_path = r""
 
 # Caminho completo com o nome do arquivo zip
-arquivo_zip_destino = r"D:\Projetos\HTML\SiteFatec.zip"
+# Full path with zip file name
+zip_file_path = r""
 
 # Compacta o diretório especificado
+# Compresses the specified directory
 zip_directory(diretorio_origem, arquivo_zip_destino)
